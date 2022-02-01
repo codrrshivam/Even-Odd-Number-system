05d7c8b025e25d918e5d87542974f01bc0d0c0cf
codrrshivam <codrrshivam@gmail.com>
2022-02-02 05:08:59 +0530

    Added Main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <isotream>
+using namespace std;
+
+int main()
+    {
+        int number,remainder;
+        cout << "Please Enter an Integer Number" : ";
+        cin >> number;
+        remainder = number % 2;
+        if( remainder == 0 ) {
+            cout << number << " is an Even Number";
+        }eles{
+            cout << number << " is a Odd Number";
+        }
+
+
+        return 0;
+    }
